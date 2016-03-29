aab1308ebe54c1f0697b590299e288c4aa435fc9
Raphaël Pinson <raphael.pinson@camptocamp.com>
2016-03-29 10:07:23 +0200

    Add spec

---
+++ spec/pdf_spec.rb
@@ -0,0 +1,17 @@
+require 'pdf/reader'
+
+describe 'Moderntimeline PDF' do
+  reader = PDF::Reader.new('moderntimeline.pdf')
+  it 'should have 11 pages' do
+    reader.page_count.should eq(3)
+  end
+  it 'should be made by TeX' do
+    reader.info[:Creator].should eq('TeX')
+  end
+  it 'should have 6 fonts on page 2' do
+    reader.pages[1].fonts.keys.size.should eq(6)
+  end
+  it 'should start with a title' do
+    reader.pages[0].text.should match('The fancytabs package')
+  end
+end
